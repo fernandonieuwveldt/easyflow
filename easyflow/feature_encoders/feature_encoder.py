478786cfd8c750ce4019df9282a02774d431683f
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2021-04-02 10:03:25 +0200

    adapted categorical and embedding class

--- easyflow/feature_encoders/feature_encoder.py
+++ easyflow/feature_encoders/feature_encoder.py
@@ -6,6 +6,31 @@ import tensorflow as tf
 from .base import BaseFeatureColumnEncoder
 
 
+def get_unique_vocab(dataset=None, features=None):
+    """Get feature vocab and create inputs
+
+    Args:
+        dataset (tf.data.Dataset): Features Data to apply encoder on.
+        features (list): list of feature names
+
+    Returns:
+        (dict, list): Keras inputs for each feature and list of encoders
+    """
+    if hasattr(dataset, '_batch_size'):
+        # unbatch dataset
+        dataset = dataset.unbatch()
+
+    feature_vocab_list, categorical_inputs = {}, {}
+    for feature in features:
+        categorical_inputs[feature] = tf.keras.Input(shape=(1,), name=feature, dtype=tf.string)
+        feature_ds = dataset.map(lambda x, y: x[feature])\
+                            .apply(tf.data.experimental.unique())\
+                            .as_numpy_iterator()
+        uniq_vocab = list(feature_ds)
+        feature_vocab_list[feature] = tf.feature_column.categorical_column_with_vocabulary_list(feature, uniq_vocab)
+    return categorical_inputs, feature_vocab_list
+
+
 class CategoricalFeatureEncoder(BaseFeatureColumnEncoder):
     """
     Class encodes Categorical features using tensorflow feature_columns
@@ -23,16 +48,8 @@ class CategoricalFeatureEncoder(BaseFeatureColumnEncoder):
         Returns:
             (dict, list): Keras inputs for each feature and list of encoders
         """
-        feature_vocab_list, categorical_inputs, feature_encoders = {}, {}, {}
-
-        for feature in features:
-            categorical_inputs[feature] = tf.keras.Input(shape=(1,), name=feature, dtype=tf.string)
-            feature_ds = dataset.map(lambda x, y: x[feature])\
-                                .apply(tf.data.experimental.unique())\
-                                .as_numpy_iterator()
-            uniq_vocab = list(feature_ds)
-            feature_vocab_list[feature] = tf.feature_column.categorical_column_with_vocabulary_list(feature, uniq_vocab)
-            feature_encoders[feature] = tf.feature_column.indicator_column(feature_vocab_list[feature])
+        categorical_inputs, feature_vocab_list = get_unique_vocab(dataset, features)
+        feature_encoders = {feature: tf.feature_column.indicator_column(feature_vocab_list[feature]) for feature in features}
         return categorical_inputs, [feature for _, feature in feature_encoders.items()]
 
 
@@ -57,18 +74,12 @@ class EmbeddingFeatureEncoder(BaseFeatureColumnEncoder):
         Returns:
             (dict, list): Keras inputs for each feature and list of encoders
         """
-        feature_vocab_list, embedding_inputs, feature_encoders = {}, {}, {}
-
-        for feature in features:
-            embedding_inputs[feature] = tf.keras.Input(shape=(1,), name=feature, dtype=tf.string)
-            feature_ds = dataset.map(lambda x, y: x[feature])\
-                                .apply(tf.data.experimental.unique())\
-                                .as_numpy_iterator()
-            uniq_vocab = list(feature_ds)
-            feature_vocab_list[feature] = tf.feature_column.categorical_column_with_vocabulary_list(feature, uniq_vocab)
-            feature_encoders[feature] = tf.feature_column.embedding_column(feature_vocab_list[feature],
-                                                                           initializer=self.initializer,
-                                                                           dimension=min(int(len(uniq_vocab)**self.embedding_space_factor), self.max_dimension))
+        embedding_inputs, feature_vocab_list = get_unique_vocab(dataset, features)
+        dimension_fn = lambda vocab: min(int(len(vocab)**self.embedding_space_factor), self.max_dimension)
+        feature_encoders = {feature:tf.feature_column.embedding_column(feature_vocab_list[feature],
+                                                                       initializer=self.initializer,
+                                                                       dimension=dimension_fn(feature_vocab_list[feature]))\
+                            for feature in features}
         return embedding_inputs, [feature for _, feature in feature_encoders.items()]
 
 
--- easyflow/feature_encoders/transformer.py
+++ easyflow/feature_encoders/transformer.py
@@ -13,11 +13,11 @@ class FeatureColumnTransformer:
     def __init__(self, feature_encoder_list=None):
         self.feature_encoder_list = feature_encoder_list
         
-    def transform(self, X):
+    def transform(self, dataset):
         """Apply feature encodings on supplied list
 
         Args:
-            X (pandas.DataFrame): Features Data to apply encoder on.
+            dataset (tf.data.Dataset): Features Data to apply encoder on.
 
         Returns:
             (dict, list): Keras inputs for each feature and list of encoders
@@ -25,7 +25,7 @@ class FeatureColumnTransformer:
         feature_layer_inputs = {}
         feature_encoders = {}
         for (name, encoder, features) in self.feature_encoder_list:
-            feature_inputs, feature_encoded = encoder.encode(X, features)
+            feature_inputs, feature_encoded = encoder.encode(dataset, features)
             feature_layer_inputs.update(feature_inputs)
             feature_encoders[name] = feature_encoded
         return feature_layer_inputs, feature_encoders
