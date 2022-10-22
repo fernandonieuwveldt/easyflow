a2029d761bf756d6c9227f5df90561a0c6a09aae
fernando <fdnieuwveldt@gmail.com>
2022-10-22 22:15:03 +0200

    update from input object to input layer

--- easyflow/preprocessing/custom.py
+++ easyflow/preprocessing/custom.py
@@ -242,7 +242,7 @@ def StringToIntegerLookup(**kwargs):
         kwargs: All arguments are related to IntegerLookup
     """
     return PreprocessorChain(
-            [tf.keras.Input(shape=(1,), dtype=tf.string), 
+            [tf.keras.InputLayer(shape=(1,), dtype=tf.string), 
              layers.StringLookup(),
              layers.IntegerLookup(output_mode='multi_hot', **kwargs)]   
     )
